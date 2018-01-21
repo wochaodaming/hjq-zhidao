4019520c85758059dfde0430505756a9858d0ca2
wochaodaming <gandhi19@163.com>
2018-01-21 19:18:13 +0800

    添加readme

--- src/test/java/com/baidu/hjq/HjqTest.java
+++ src/test/java/com/baidu/hjq/HjqTest.java
@@ -7,11 +7,16 @@ import org.junit.runners.BlockJUnit4ClassRunner;
 @RunWith(BlockJUnit4ClassRunner.class)
 public class HjqTest {
     @Test
-    public void testClasspath(){
+    public void testClasspath() {
         String path = System.getProperty("user.dir");
         System.out.println(path);
 
         System.out.println(System.getProperty("user.dir") + "\\src\\main\\resources\\test\\");
+    }
+
+    @Test
+    public void test() {
+        System.out.println("=-----===");
     }
 }
 
